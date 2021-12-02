fddc6483ee5fd217eba14e731081a2730fdf52ec
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-12-02 19:31:07 +0100

    feat: filter input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,34 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import repos from '@/components/data.js'
 // Fetch data from an API
-// const response = await fetch('https://open-source-info-api.herokuapp.com/api/v1/orgs/guidesmiths/repos',
-//   {
-//     method: 'GET',
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     }
-//   }
-// )
-// const repos = await response.json()
+// const response = await fetch('https://open-source-info-api.herokuapp.com/api/v1/orgs/guidesmiths/repos')
+//   .then(response => response.json())
+
+const searchText = ref('')
+const filteredRepos = ref(repos)
+
+const formatDate = date => {
+  const d = new Date(date)
+  return `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`
+}
+
+const filterRepos = () => {
+  const search = searchText.value.toLowerCase()
+  filteredRepos.value = repos.filter(repo => {
+    repo.description = repo.description || ''
+    return repo.name.toLowerCase().includes(search) || repo.description.toLowerCase().includes(search)
+  })
+}
+
+const onChange = () => {
+  if (searchText.value.length > 0) {
+    filterRepos()
+  } else {
+    filteredRepos.value = repos
+  }
+}
+
 
 </script>
 
@@ -25,13 +42,41 @@ import repos from '@/components/data.js'
             <p class="text-xl text-gray-500">We are open source</p>
           </div>
 
+          <div class="grid grid-cols-2 gap-4">
+            <div>
+              <div
+                class="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-pink-600 focus-within:border-pink-600"
+              >
+                <label
+                  for="search"
+                  class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+                >Search</label>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  v-model="searchText"
+                  class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                  placeholder="Search by name"
+                  @input="onChange"
+                />
+              </div>
+            </div>
+            <div class="flex items-center justify-end">
+              <p class="text-gray-500 text-xl font-medium">Showing {{ filteredRepos.length }} repos</p>
+            </div>
+          </div>
+
           <div class="bg-white shadow overflow-hidden sm:rounded-md">
             <ul role="list" class="divide-y divide-gray-200">
-              <li v-for="repo in repos" :key="repo.name">
-                <a :href="repo.url" class="block hover:bg-gray-50" :title="repo.url">
+              <li v-for="repo in filteredRepos" :key="repo.name">
+                <a :href="repo.url" class="block hover:bg-gray-50">
                   <div class="px-4 py-4 sm:px-6">
                     <div class="flex items-center justify-between">
-                      <p class="font-medium text-indigo-600 truncate">{{ repo.name }}</p>
+                      <p
+                        class="font-medium text-xl text-pink-600 truncate"
+                        :title="repo.url"
+                      >{{ repo.name }}</p>
                       <div class="ml-2 flex-shrink-0 flex space-x-4">
                         <p
                           v-if="repo.hasIssues"
@@ -48,11 +93,17 @@ import repos from '@/components/data.js'
                       </div>
                     </div>
 
-                    <p class="leading-5 text-gray-500 truncate">{{ repo.description }}</p>
-                    <div class="mt-2 sm:flex sm:justify-between">
+                    <div class="mt-2">
+                      <p
+                        class="leading-5 text-gray-500 truncate max-w-full sm:max-w-screen-sm"
+                      >{{ repo.description }}</p>
+                    </div>
+                    <div class="mt-4 sm:flex sm:justify-between">
                       <div class="sm:flex">
-                        <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:mr-4">
-                          <!-- class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" -->
+                        <p
+                          class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:mr-4"
+                          title="Size"
+                        >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             class="h-6 w-6"
@@ -95,21 +146,24 @@ import repos from '@/components/data.js'
                       <div class="flex items-center text-sm text-gray-500 sm:mt-0">
                         <!-- Heroicon name: solid/calendar -->
                         <svg
-                          class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                           xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
+                          class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
                         >
                           <path
-                            fill-rule="evenodd"
-                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                            clip-rule="evenodd"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                           />
                         </svg>
                         <p>
-                          Updated at
-                          <time :datetime="repo.updatedAt">{{ repo.updatedAt }}</time>
+                          <time
+                            :title="`Updated at ${formatDate(repo.updatedAt)}`"
+                            :datetime="repo.updatedAt"
+                          >{{ formatDate(repo.updatedAt) }}</time>
                         </p>
                       </div>
                     </div>
